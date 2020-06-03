79608aea10639eac44fb43eb26c22f15989ed7af
AliRazavi <alirazavi253@gmail.com>
2020-06-04 03:34:32 +0430

    String

---
+++ script.js
@@ -0,0 +1,29 @@
+// let directory="web";
+// let nameFile="index.html";
+
+// let val;
+
+// val=directory.concat("/",nameFile);
+
+const search=document.querySelector("#search");
+
+let username=["Ali","Mohammad","Sara","Samin","Reza","Ho3ien","Mohmood","Omid","Pouya","Pedram","Sezar","Sadegh"];
+
+
+
+search.addEventListener("keyup",(e)=>{
+
+let result;
+username.forEach(element => {
+    
+    console.log(element.charAt('A'));
+   
+    
+});
+
+
+console.log(result);
+})
+
+
+
