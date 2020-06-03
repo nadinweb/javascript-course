dbf4dd2114831e6220bcbf74a83c5fcfed0498ba
AliRazavi <alirazavi253@gmail.com>
2020-06-03 23:36:59 +0430

    end class console

---
+++ script.js
@@ -0,0 +1,31 @@
+/* console  */
+
+console.log("Hello World !");
+
+console.log([1,2,3,45,66,88,77,1100,100]);
+
+// console.clear();
+
+console.log({name:"Ali",email:"ali@gmail.com"});
+
+
+console.error("error to connect databases");
+
+console.info("inja dar hale kharab shodan hastesh !");
+
+console.warn("Hello world !");
+
+console.table({name:"Ali",family:"Razavi"});
+
+console.time("DD");
+
+console.log("Hello World !");
+console.log("Hello World !");
+console.log("Hello World !");
+console.log("Hello World !");
+console.log("Hello World !");
+console.log("Hello World !");
+console.log("Hello World !");
+
+console.timeEnd("DD");
+
