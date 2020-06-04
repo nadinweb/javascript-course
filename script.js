616512461473bf0fcec2a9d51e69fa06fabe705b
AliRazavi0 <razavi.ali.1998@gmail.com>
2020-06-04 07:35:24 -0700

    function arrowfunction

---
+++ script.js
@@ -0,0 +1,22 @@
+(function(firstName){
+    console.log(firstName);
+    
+})('Ali');
+
+
+let todo={
+    title:"this is title",
+    addTodo:(input)=>{
+        return `save todo : ${input}`
+    },
+}
+
+function greating(){
+    console.log("Hello World !");
+    
+}
+
+let greatingWithUserName=(username)=>{
+    console.log(`Hello ${username}`);
+}
+
