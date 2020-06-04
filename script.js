03b174b1df5ee735613c8e1d4fe0d6c5d2d31be3
AliRazavi0 <razavi.ali.1998@gmail.com>
2020-06-04 06:57:57 -0700

    if switch

---
+++ script.js
@@ -0,0 +1,53 @@
+let username="Admin";
+let password="1234567";
+
+
+if(username === "Admin" && password=="123456789"){
+    console.log("welcome admin to my page");
+}
+else
+{
+    console.log("you are just user  !");
+    
+}
+
+// let day=new Date().getDay();
+let day=51;
+
+
+switch(day){
+    case  0 :
+        console.log("1 shanbe");
+        
+    break;
+
+    case  1 :
+        console.log("2 shanbe");
+        
+    break;
+    case  2 :
+        console.log("3 shanbe");
+        
+    break;
+    case  3 :
+        console.log("4 shanbe");
+        
+    break;
+    case  4 :
+        console.log("5 shanbe");
+        
+    break;
+    case 5 :
+        console.log("jomeh");
+        
+    break;
+    case 6 :
+        console.log("shanbeh");
+        
+    break;
+
+    default :
+    console.log("invalid !");
+    
+    break
+}
