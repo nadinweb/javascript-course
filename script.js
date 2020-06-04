3f77b301561a91769b81d8357fe1c3133d52906b
AliRazavi0 <razavi.ali.1998@gmail.com>
2020-06-04 05:05:08 -0700

    Array

---
+++ script.js
@@ -0,0 +1,22 @@
+let username="Ali";
+let blog=`this is blog text form ${username}`;
+
+const numbers=[-20,20,30,50,-1,20,0,11,-80,80,99,100,220,55,66,44,11,88,77,22,0,9,6,-1];
+
+
+let val;
+
+// val=numbers.sort();
+
+// val=numbers.splice(1,10);
+// val=numbers.toLocaleString();
+// val=numbers.toString();
+// val=numbers.shift(1000);
+
+// val=numbers.filter(function(num){
+//     return num<10;
+// })
+
+
+console.log(val);
+// console.log(numbers);
